d93bf1e5c98f53d7d51f44697584f43ac9c398a7
pavel krusek <pavel.krusek@gmail.com>
2009-08-09 20:10:56 +0200

    change environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 ENV['RAILS_ENV'] ||= 'development'
 
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
 
